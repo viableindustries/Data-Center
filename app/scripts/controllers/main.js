12df7cf1881e815a096eaab348e1b1d6a9242d4c
Joshua Powell <joshua@viableindustries.com>
2014-05-14 08:37:15 -0400

    Added a check to see if we're in the development or production environment;

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,9 +11,16 @@ angular.module('commonsCloudAdminApp')
       //
       // Default Redirect for authnetication, not this is the development URL and Client ID
       //
+      console.log('host', host);
+      debugger;
+
       if (host === 'localhost' || host === '127.0.0.1') {
+        console.log('chose localhost or 127.0.0.1', host);
+        debugger;
         $window.location.href = 'http://api.commonscloud.org/oauth/authorize?response_type=token&client_id=PGvNp0niToyRspXaaqx3PiQBMn66QXyAq5yrNHpz&redirect_uri=http%3A%2F%2F127.0.0.1%3A9000%2Fauthorize&scope=user applications';
       } else {
+        console.log('chose app.commonscloud.org', host);
+        debugger;
         $window.location.href = 'http://api.commonscloud.org/oauth/authorize?response_type=token&client_id=MbanCzYpm0fUW8md1cdSJjUoYI78zTbak2XhZ2hF&redirect_uri=http%3A%2F%2Fapp.commonscloud.org%2Fauthorize&scope=user applications';
       }
 
@@ -21,5 +28,6 @@ angular.module('commonsCloudAdminApp')
       $location.hash('');
       $location.path('/applications');
     }
-    
+
+
   }]);

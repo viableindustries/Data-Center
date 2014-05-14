f7dae93a272b0ad00a1cb9aba9b65f4fc1b69e6c
Joshua Powell <joshua@viableindustries.com>
2014-05-14 08:34:04 -0400

    Added a check to see if we're in the development or production environment;

--- dist/scripts/229539c8.scripts.js
+++ dist/scripts/229539c8.scripts.js
@@ -690,15 +690,25 @@ angular
 angular.module('commonsCloudAdminApp')
   .controller('MainCtrl', ['$rootScope', '$scope', 'ipCookie', '$location', '$window', function($rootScope, $scope, ipCookie, $location, $window) {
 
+    var host = $location.host();
     var session_cookie = ipCookie('session');
 
     if (!session_cookie) {
-      $window.location.href = 'http://api.commonscloud.org/oauth/authorize?response_type=token&client_id=PGvNp0niToyRspXaaqx3PiQBMn66QXyAq5yrNHpz&redirect_uri=http%3A%2F%2F127.0.0.1%3A9000%2Fauthorize&scope=user applications';
+
+      //
+      // Default Redirect for authnetication, not this is the development URL and Client ID
+      //
+      if (host === 'localhost' || host === '127.0.0.1') {
+        $window.location.href = 'http://api.commonscloud.org/oauth/authorize?response_type=token&client_id=PGvNp0niToyRspXaaqx3PiQBMn66QXyAq5yrNHpz&redirect_uri=http%3A%2F%2F127.0.0.1%3A9000%2Fauthorize&scope=user applications';
+      } else {
+        $window.location.href = 'http://api.commonscloud.org/oauth/authorize?response_type=token&client_id=MbanCzYpm0fUW8md1cdSJjUoYI78zTbak2XhZ2hF&redirect_uri=http%3A%2F%2Fapp.commonscloud.org%2Fauthorize&scope=user applications';
+      }
+
     } else {
       $location.hash('');
       $location.path('/applications');
     }
-
+    
   }]);
 
 'use strict';

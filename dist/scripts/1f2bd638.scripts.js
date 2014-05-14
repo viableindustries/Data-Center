823142908fc62b54b4bf4327a4c19ad88887d972
Joshua Powell <joshua@viableindustries.com>
2014-05-14 08:14:22 -0400

    Fixes bug in the Photo upload User Interface and adds new fields for caption, credit and credit link; Fixes bug in the Relationships drop down preventing new Relationship Fields to be saved and throwing a 500 error;

--- dist/scripts/1f2bd638.scripts.js
+++ dist/scripts/1f2bd638.scripts.js
@@ -1297,9 +1297,6 @@ angular.module('commonsCloudAdminApp')
         };
       });
 
-      console.log('files', $scope.files);
-
-
       // $scope.dataUrls = [];
       // $scope.file_list = [];
       //$files: an array of files selected, each file has name, size, and type.
@@ -1343,10 +1340,6 @@ angular.module('commonsCloudAdminApp')
       //   //.then(success, error, progress); 
       //   //.xhr(function(xhr){xhr.upload.addEventListener(...)})// access and attach any event listener to XMLHttpRequest.
       // }
-
-
-
-      console.log('$scope.files', $scope.files);
     };
 
 

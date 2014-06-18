632b0b11ab5ca12b5090d5c9e82d07aee4da909e
Joshua Powell <joshua@viableindustries.com>
2014-06-18 11:02:05 -0400

    Added new pages for Create Application and Create Template along with their individual controllers to break everything out nicely;

---
+++ app/scripts/controllers/application-create.js
@@ -0,0 +1,70 @@
+'use strict';
+
+angular.module('commonsCloudAdminApp')
+  .controller('ApplicationCreateCtrl', ['$rootScope', '$scope', 'Application', '$location', function ($rootScope, $scope, Application, $location) {
+
+    //
+    // Instantiate an Application object so that we can perform all necessary
+    // functionality against our Application resource
+    //
+    $scope.application = new Application();
+    $scope.applications = Application.query();
+
+    //
+    // Start a new Alerts array that is empty, this clears out any previous
+    // messages that may have been presented on another page
+    //
+    $rootScope.alerts = ($rootScope.alerts) ? $rootScope.alerts: [];
+
+    //
+    // Define the Breadcrumbs that appear at the top of the page in the nav bar
+    //
+    $scope.breadcrumbs = [
+      {
+        'label': 'Applications',
+        'title': 'View my applications',
+        'url': '/applications',
+        'class': 'active'
+      }
+    ];
+
+    //
+    // Save a new Application to the API Database
+    //
+    $scope.save = function () {
+
+      //
+      // Save the Application via a post to the API and then push it onto the
+      // Applications array, so that it appears in the user interface
+      //
+      $scope.application.$save().then(function (response) {
+        $scope.applications.push(response.response);
+
+        var alert = {
+          'type': 'success',
+          'title': 'Sweet!',
+          'details': 'Your new Application was created, go add some stuff to it.'
+        };
+
+        $rootScope.alerts.push(alert);
+        $location.path('/applications');
+
+      }, function (error) {
+        //
+        // Once the template has been updated successfully we should give the
+        // user some on-screen feedback and then remove it from the screen after
+        // a few seconds as not to confuse them or force them to reload the page
+        // to dismiss the message
+        //
+        var alert = {
+          'type': 'error',
+          'title': 'Oops!',
+          'details': 'Looks like we couldn\'t create that Application, mind trying again?'
+        };
+
+        $scope.alerts.push(alert);
+      });
+
+    };
+
+  }]);
---
+++ app/scripts/controllers/template-create.js
@@ -0,0 +1,106 @@
+'use strict';
+
+angular.module('commonsCloudAdminApp')
+  .controller('TemplateCreateCtrl', ['$rootScope', '$routeParams', '$scope', 'Application', 'Template', '$location', function ($rootScope, $routeParams, $scope, Application, Template, $location) {
+
+    //
+    // Instantiate an Application object so that we can perform all necessary
+    // functionality against our Application resource
+    //
+    $scope.application = {};
+    $scope.newTemplate = new Template();
+
+    //
+    // Start a new Alerts array that is empty, this clears out any previous
+    // messages that may have been presented on another page
+    //
+    $rootScope.alerts = ($rootScope.alerts) ? $rootScope.alerts: [];
+
+    //
+    // Define the Breadcrumbs that appear at the top of the page in the nav bar
+    //
+    $scope.breadcrumbs = [
+      {
+        'label': 'Applications',
+        'title': 'View my applications',
+        'url': '/applications',
+        'class': ''
+      }
+    ];
+
+    //
+    // Get the application the user has selected and begin loading the rest of
+    // the application page
+    //
+    $scope.GetApplication = function() {
+      //
+      // Get the single application that the user wants to view
+      //
+      Application.get({
+          id: $routeParams.applicationId
+        }).$promise.then(function(response) {
+
+          //
+          // Assign the response to the Application object and end loading
+          //
+          $scope.application = response.response;
+
+          //
+          // Update the breadcrumbs based on the response from the application
+          //
+          $scope.breadcrumbs.push({
+            'label': $scope.application.name,
+            'title': 'View ' + $scope.application.name,
+            'url': '/applications/' + $scope.application.id,
+            'class': ''
+          });
+
+          $scope.breadcrumbs.push({
+            'label': 'Feature Collections',
+            'title': 'View all of ' + $scope.application.name + '\'s feature collections',
+            'url': '/applications/' + $scope.application.id,
+            'class': ''
+          });
+
+          $scope.breadcrumbs.push({
+            'label': 'New',
+            'title': '',
+            'url': '/applications/' + $scope.application.id + '/collections/new',
+            'class': 'active'
+          });
+
+        }, function(error) {
+          $rootScope.alerts.push({
+            'type': 'error',
+            'title': 'Uh-oh!',
+            'details': 'Mind reloading the page? It looks like we couldn\'t get that Application for you.'
+          });
+        });
+    };
+
+    $scope.GetApplication();
+
+    //
+    // Create a new Template that does not yet exist in the API database
+    //
+    $scope.CreateTemplate = function() {
+      $scope.newTemplate.$save({
+        applicationId: $scope.application.id
+      }).then(function(response) {
+        $rootScope.alerts.push({
+          'type': 'success',
+          'title': 'Great!',
+          'details': 'We built that Template for you, now add some Fields to it.'
+        });
+
+        $location.path('/applications/' + $scope.application.id);
+      }, function(error) {
+        $rootScope.alerts.push({
+          'type': 'error',
+          'title': 'Uh-oh!',
+          'details': 'Mind trying that again? It looks like we couldn\'t save that Template for you.'
+        });
+      });
+    };
+
+  }]);

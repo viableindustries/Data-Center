e3b4f7a70f4cd8a04c86f2993d82374c92126e99
Joshua Powell <joshua@viable.io>
2014-09-22 17:37:57 -0400

    Added staging site loging information;

--- app/scripts/controllers/index.js
+++ app/scripts/controllers/index.js
@@ -13,6 +13,8 @@ angular.module('commonsCloudAdminApp')
       //
       if (host === 'localhost' || host === '127.0.0.1') {
         $scope.login_url = 'http://api.commonscloud.org/oauth/authorize?response_type=token&client_id=PGvNp0niToyRspXaaqx3PiQBMn66QXyAq5yrNHpz&redirect_uri=http%3A%2F%2F127.0.0.1%3A9000%2Fauthorize&scope=user applications';
+      } else if (host === 'stg.commonscloud.org') {
+        $scope.login_url = 'http://api.commonscloud.org/oauth/authorize?response_type=token&client_id=MbanCzYpm0fUW8md1cdSJjUoYI78zTbak2XhZ2hf&redirect_uri=http%3A%2F%2Fstg.commonscloud.org%2Fauthorize&scope=user applications';
       } else {
         $scope.login_url = 'http://api.commonscloud.org/oauth/authorize?response_type=token&client_id=MbanCzYpm0fUW8md1cdSJjUoYI78zTbak2XhZ2hF&redirect_uri=http%3A%2F%2Fapp.commonscloud.org%2Fauthorize&scope=user applications';
       }
